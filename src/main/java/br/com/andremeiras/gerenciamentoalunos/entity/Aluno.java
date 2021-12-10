5780a90c71a502dd553a2a70410b91f4a0278f61
André Meira <andremeirati@gmail.com>
2021-12-09 23:51:54 -0300

    Criacao da entidade no banco de dados

---
+++ src/main/java/br/com/andremeiras/gerenciamentoalunos/entity/Aluno.java
@@ -0,0 +1,80 @@
+package br.com.andremeiras.gerenciamentoalunos.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "aluno")
+public class Aluno {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "seq_aluno")
+	private Long id;
+
+	@Column(name = "nome", nullable = false)
+	private String nome;
+
+	@Column(name = "sobrenome")
+	private String sobrenome;
+
+	@Column(name = "email")
+	private String email;
+
+	@Column(name = "idade")
+	private int idade;
+
+	public Aluno() {
+	}
+
+	public Aluno(String nome, String sobrenome, String email, int idade) {
+		this.nome = nome;
+		this.sobrenome = sobrenome;
+		this.email = email;
+		this.idade = idade;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getSobrenome() {
+		return sobrenome;
+	}
+
+	public void setSobrenome(String sobrenome) {
+		this.sobrenome = sobrenome;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public int getIdade() {
+		return idade;
+	}
+
+	public void setIdade(int idade) {
+		this.idade = idade;
+	}
+
+}
